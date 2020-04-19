4acf6d6bc1c24a8f61cb8284df3a9b9443319607
Lavinia Kanagandran <lavinia.kanagandran@rokt.com>
2020-04-19 17:41:47 +1000

    Add charts successfully

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import json
 
 from datetime import datetime
 from google.oauth2 import service_account
@@ -8,12 +9,12 @@ from utils import create_a_copy, generate_slide_requests, generate_sheet_request
 
 datetime_now = datetime.now()
 
-generated_content_folder_id = '1xhk_PfBuXjib-nQUtCo_266RkD2k6cFR'  # Pre-created Google Drive file
+FOLDER_ID = '1xhk_PfBuXjib-nQUtCo_266RkD2k6cFR'  # Pre-created Google Drive file
+TEMPLATE_PRESENTATION_ID = '1i3jbxK48AQmDMdYfdslPd3Emn7ydpCutiG9lVY32V8E'
+TEMPLATE_SPREADSHEET_ID = '1Q0NtNj3dkznONXEv8y8xaQU5cHwjw7aOYjYyCIRNesQ'
 
-REPORT_TEMPLATE = 'HealthCheckReport_Template'
-REPORT_CHART_TEMPLATE = 'HealthCheckReport_Charts'
 
-# Google Auth
+### Google Auth ###
 SCOPES = (
     'https://www.googleapis.com/auth/drive',
     'https://www.googleapis.com/auth/presentations',
@@ -27,84 +28,25 @@ credentials = service_account.Credentials.from_service_account_file(
 DRIVE = discovery.build('drive',  'v3', credentials=credentials)
 SLIDES = discovery.build('slides', 'v1', credentials=credentials)
 SHEETS = discovery.build('sheets', 'v4', credentials=credentials)
+####################
 
-# 1. Duplicate main report Google Slides
-REPORT_ID = create_a_copy(REPORT_TEMPLATE, generated_content_folder_id, DRIVE)
 
-# 2. Duplicate report charts Google Sheet
-CHARTS_ID = create_a_copy(REPORT_CHART_TEMPLATE, generated_content_folder_id, DRIVE)
+# 1. Duplicate report charts Google Sheet
+NEW_SPREADSHEET_ID = create_a_copy(TEMPLATE_SPREADSHEET_ID, FOLDER_ID, DRIVE)
 
-# 3. Get values from form (currently hardcoded)
-# TODO: Get from form
+# 2. Update Google Sheet chart values
+print('Updating Google Sheet chart values')
+body = generate_sheet_requests()
+SHEETS.spreadsheets().values().batchUpdate(spreadsheetId=NEW_SPREADSHEET_ID, body=body).execute()
+spreadsheet = SHEETS.spreadsheets().get(spreadsheetId=NEW_SPREADSHEET_ID).execute()
+sheets = spreadsheet.get("sheets")
 
-fill_values = {
-    'COMPANY_NAME': 'Random Company',
-    'MONTH': datetime_now.strftime('%B'),
-    'YEAR': datetime_now.strftime('%Y'),
-    'DATE': datetime_now.strftime('%d %B %Y'),
-    'USER': 'TestUser',
-    'TCH_WMN': '14',
-    'TCH_WMN_EVAL': 'low',
-    'ENG_WMN': '4',
-    'INF_WMN': '6',
-    'DAT_WMN': '20',
-    'PRD_WMN': '9',
-    'SEC_WMN': '0',
-    'TECHLEAD_WMN': '2',
-    'LDRSHIP_WMN': '25',
-    'SNR_LDRSHIP_WMN': '50',
-    'PRMY_CARER_LEAVE_WKS': '8',
-    'SEC_CARER_LEAVE_WKS': '2',
-    'QLFYNG_PERIOD_MNTHS': '12',
-    'TCH_MEN': '86'
-}
+# 3. Duplicate main report Google Slides
+NEW_PRESENTATION_ID = create_a_copy(TEMPLATE_PRESENTATION_ID, FOLDER_ID, DRIVE)
 
-chart_fill_values = {
-    'ENG': ['ENG_WMN', 50],
-    'DATA': ['DAT_WMN', 50],
-    'PRODUCT': ['PRD_WMN', 50],
-    'INFRA': ['INF_WMN', 50],
-    'SECURITY': ['SEC_WMN', 50],
-    'TECH_LEADS': ['TECHLEAD_WMN', 50],
-    'TECH_LEADERSHIP': ['LDRSHIP_WMN', 50],
-    'CO_LEADERSHIP_TEAM': ['SNR_LDRSHIP_WMN', 50],
-    'OVERALL_TECH': ['TCH_WMN', 'TCH_MEN']
-}
-
-# 4. Update Google Sheet chart values
-print('4. Update Google Sheet chart values')
-body = generate_sheet_requests(chart_fill_values, fill_values)
-SHEETS.spreadsheets().values().batchUpdate(spreadsheetId=CHARTS_ID, body=body).execute()
-
-# 5. Update Google Slides
-print('5. Update Google Slides')
-reqs = generate_slide_requests(fill_values)
-SLIDES.presentations().batchUpdate(body={'requests': reqs}, presentationId=REPORT_ID).execute()
+# 4. Update Google Slides
+print('Updating Google Slides')
+reqs = generate_slide_requests(NEW_SPREADSHEET_ID)
+SLIDES.presentations().batchUpdate(body={'requests': reqs}, presentationId=NEW_PRESENTATION_ID).execute()
 
 print('Process completed!')
-
-# print('** Get slide objects, search for image placeholder')
-# slide = SLIDES.presentations().get(presentationId=DECK_ID,
-#         fields='slides').execute().get('slides')[0]
-# obj = None
-# for obj in slide['pageElements']:
-#     if obj['shape']['shapeType'] == 'RECTANGLE':
-#         break
-#
-# print('** Searching for icon file')
-# rsp = DRIVE.files().list(q="name='%s'" % IMG_FILE).execute().get('files')[0]
-# print(' - Found image %r' % rsp['name'])
-# img_url = '%s&access_token=%s' % (
-#         DRIVE.files().get_media(fileId=rsp['id']).uri, creds.access_token)
-#
-# # reqs = [
-# #     # {'createImage': {
-# #     #     'url': img_url,
-# #     #     'elementProperties': {
-# #     #         'pageObjectId': slide['objectId'],
-# #     #         'size': obj['size'],
-# #     #         'transform': obj['transform'],
-# #     #     }
-# #     # }},
-# #     # {'deleteObject': {'objectId': obj['objectId']}},
-# # ]
--- utils.py
+++ utils.py
@@ -1,35 +1,159 @@
 from datetime import datetime
 
+datetime_now = datetime.now()
 
-def create_a_copy(unique_src_filename_substr, dest_folder_id, drive_api):
+fill_values = {
+    'COMPANY_NAME': 'Random Company',
+    'MONTH': datetime_now.strftime('%B'),
+    'YEAR': datetime_now.strftime('%Y'),
+    'DATE': datetime_now.strftime('%d %B %Y'),
+    'USER': 'TestUser',
+    'TCH_WMN': '14',
+    'TCH_WMN_EVAL': 'low',
+    'ENG_WMN': '4',
+    'INF_WMN': '0',
+    'DAT_WMN': '20',
+    'PRD_WMN': '9',
+    'SEC_WMN': '0',
+    'TECHLEAD_WMN': '2',
+    'LDRSHIP_WMN': '25',
+    'SNR_LDRSHIP_WMN': '50',
+    'PRMY_CARER_LEAVE_WKS': '8',
+    'SEC_CARER_LEAVE_WKS': '2',
+    'QLFYNG_PERIOD_MNTHS': '12',
+    'TCH_MEN': '86'
+}
+
+chart_fill_values = {
+    'ENG': ['ENG_WMN', 50],
+    'DATA': ['DAT_WMN', 50],
+    'PRODUCT': ['PRD_WMN', 50],
+    'INFRA': ['INF_WMN', 50],
+    'SECURITY': ['SEC_WMN', 0],
+    'TECH_LEADS': ['TECHLEAD_WMN', 50],
+    'TECH_LEADERSHIP': ['LDRSHIP_WMN', 50],
+    'CO_LEADERSHIP_TEAM': ['SNR_LDRSHIP_WMN', 50],
+    'OVERALL_TECH': ['TCH_WMN', 50]
+}
+
+charts = {
+    "g5784b994ba_3_0": {
+        'chartId': 715383549,  # Security Chart
+        'transform':
+        {
+            "scaleX": 105,
+            "scaleY": 105,
+            "translateX": 1822649.67,
+            "translateY": 4691775,
+            "unit": "EMU"
+        }
+    },
+    "g5784b994ba_0_0": {
+        'chartId': 1334235346,  # Infrastructure Chart
+        'transform':
+        {
+            "scaleX": 105,
+            "scaleY": 105,
+            "translateX": 3865363,
+            "translateY": 1978925,
+            "unit": "EMU"
+        }
+    },
+    "g63b241fd6a_0_0": {
+        'chartId': 353279936,  # Data Chart
+        'transform':
+        {
+            "scaleX": 105,
+            "scaleY": 105,
+            "translateX": 5908077,
+            "translateY": 1978925,
+            "unit": "EMU"
+        }
+    },
+    "g54cefca8fe_0_87": {
+        'chartId': 607026404,  # Co Senior Leadership Team Chart
+        'transform':
+        {
+            "scaleX": 105,
+            "scaleY": 105,
+            "translateX": 7950791,
+            "translateY": 4691775,
+            "unit": "EMU"
+        }
+    },
+    "g54cefca8fe_0_88": {
+        'chartId': 557365308,  # Tech Leadership Chart
+        'transform':
+        {
+            "scaleX": 105,
+            "scaleY": 105,
+            "translateX": 5908077,
+            "translateY": 4691775,
+            "unit": "EMU"
+        }
+    },
+    "g54cefca8fe_0_89": {
+        'chartId': 1658241655,  # Tech Leads Chart
+        'transform':
+        {
+            "scaleX": 105,
+            "scaleY": 105,
+            "translateX": 3865363,
+            "translateY": 4691775,
+            "unit": "EMU"
+        }
+    },
+    "g54cefca8fe_0_90": {
+        'chartId': 1365614958,  # Engineering Chart
+        'transform':
+        {
+            "scaleX": 105,
+            "scaleY": 105,
+            "translateX": 1822649,
+            "translateY": 1978925,
+            "unit": "EMU"
+        }
+    },
+    "g54cefca8fe_0_92": {
+        'chartId': 513287435,  # Product Management Chart
+        'transform':
+        {
+            "scaleX": 105,
+            "scaleY": 105,
+            "translateX": 7950791,
+            "translateY": 1978925,
+            "unit": "EMU"
+        }
+    },
+
+}
+
+reqs = []
+
+
+
+def create_a_copy(file_id, dest_folder_id, drive_api):
 
     filename_datetime_format = '%Y%m%d_%H%M%S'
     datetime_now = datetime.now()
 
     # TODO: Add error handling
-
-    # Get ID of source file
-    response = drive_api.files().list(q="name contains '%s'" % unique_src_filename_substr).execute()
-
-    # TODO: check file count
-    src_file = response.get('files')[0]
-
     # Generate payload for new file
     dest_file_name = f'{datetime_now.strftime(filename_datetime_format)}_HealthCheckReport'
     payload = {'name': dest_file_name,
                'parents': [dest_folder_id]}
 
     # Copy file
-    print('** Copying template %r as %r' % (src_file['name'], dest_file_name))
-    dest_file_id = drive_api.files().copy(body=payload, fileId=src_file['id']).execute().get('id')
+    print('** Copying template %r as %r' % (file_id, dest_file_name))
+    dest_file_id = drive_api.files().copy(body=payload, fileId=file_id).execute().get('id')
     print(f'** Successfully made a copy | FILE NAME: {dest_file_name}  |  FILE ID: {dest_file_id}')
 
     return dest_file_id
 
 
-def generate_slide_requests(fill_values):
+def generate_slide_requests(new_spreadsheet_id):
     reqs = []
-    # TODO: Update charts
+
     for key, value in fill_values.items():
         req_object = \
             {
@@ -41,10 +165,36 @@ def generate_slide_requests(fill_values):
             }
         reqs.append(req_object)
 
+    for object_id, chart in charts.items():
+        req = \
+            {
+                "createSheetsChart": {
+                    "objectId": object_id,
+                    "elementProperties": {
+                        "pageObjectId": "g54cefca8fe_0_86",
+                        "size": {
+                            "width": {
+                                "magnitude": 30000,
+                                "unit": "EMU"
+                            },
+                            "height": {
+                                "magnitude": 18550,
+                                "unit": "EMU"
+                            }
+                        },
+                        "transform": chart['transform']
+                    },
+                  "spreadsheetId": new_spreadsheet_id,
+                  "chartId": chart['chartId'],
+                  "linkingMode": "LINKED"
+                }
+            }
+        reqs.append(req)
+
     return reqs
 
 
-def generate_sheet_requests(chart_fill_values, fill_values):
+def generate_sheet_requests():
     data = []
     for sheet, values in chart_fill_values.items():
         value_range = {
